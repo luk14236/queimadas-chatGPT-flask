90df0f368f4652774adf923929134a4c314c3356
Lucas Oliveira <luk14236@gmail.com>
2024-06-26 00:06:48 +0200

    Treinando o Heldinho o guru do inpe queimadas

--- app/app.py
+++ app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
+import dotenv
 import openai
+import os
 
-# Configure a sua chave de API da OpenAI
-openai.api_key = 'sua_chave_de_api_aqui'
+env_vars = dotenv.dotenv_values()
+openai.api_key = env_vars["openai-key"]
 
 app=Flask(__name__, template_folder='../templates', static_folder='../static')
 
@@ -15,14 +17,27 @@ def get_response():
     user_message = request.form['user_message']
     
     # Chame a API do GPT-3 para gerar uma resposta
-    response = openai.Completion.create(
-        engine="text-davinci-003",  # Escolha o mecanismo de linguagem adequado
-        prompt=user_message,
-        max_tokens=50  # Número máximo de tokens na resposta
-    )
-    
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages = [
+        {
+            "role": "system",
+            "content": "Voce dever estar pronto para responder qualquer pergunta sobre instituto nacional de pesquisas espaciais, todas as perguntas sao sobre o mesmo e vc deve se identificar como Heldinho."
+        },    
+        {
+            "role": "user",
+            "content": user_message,
+        }
+    ],
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )  
+
+    print(response)
     # Pegue a resposta gerada pelo modelo
-    chat_response = response.choices[0].text.strip()
+    chat_response = response.choices[0].message.content
     
     return chat_response
 
