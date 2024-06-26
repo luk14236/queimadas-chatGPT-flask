ad5b9bcedbd8b4718219c6fe77eb94a601d805d3
Lucas Oliveira <luk14236@gmail.com>
2024-06-26 22:36:32 +0200

    Enviando o nosso chat Heldinho com o front e lembrando da conversa

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json 
 import dotenv
 import openai
 import os
@@ -15,28 +16,28 @@ def index():
 @app.route('/get-response', methods=['POST'])
 def get_response():
     user_message = request.form['user_message']
+
+    mensagens_geral = [
+        {
+            "role": "system",
+            "content": "Voce dever estar pronto para responder qualquer pergunta sobre instituto nacional de pesquisas espaciais, todas as perguntas sao sobre o mesmo e vc deve se identificar como Heldinho."
+        }
+    ]
+
+    messages = json.loads(user_message)
+
+    mensagens_geral.extend(messages)
     
     # Chame a API do GPT-3 para gerar uma resposta
     response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages = [
-        {
-            "role": "system",
-            "content": "Voce dever estar pronto para responder qualquer pergunta sobre instituto nacional de pesquisas espaciais, todas as perguntas sao sobre o mesmo e vc deve se identificar como Heldinho."
-        },    
-        {
-            "role": "user",
-            "content": user_message,
-        }
-    ],
+        messages = json.loads(json.dumps(mensagens_geral)),
         max_tokens=150,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.2,
     )  
 
-    print(response)
-    # Pegue a resposta gerada pelo modelo
     chat_response = response.choices[0].message.content
     
     return chat_response
